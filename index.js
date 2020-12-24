186c245a6877271f0cb309ad0710515baf9e54e7
Ibrahim Nour <ibrahamnour22@gmail.com>
2020-12-24 12:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Circle{
       this.radius=diameter/2;
     }
     set circumference(circumference){
-      this.circumference=circumference;
+      this.radius=circumference/(2*Math.PI);
     }
 }
   
